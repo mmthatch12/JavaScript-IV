f4e36ad3e46df4ef0348e7b3076fb83c59020ed3
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-06-13 13:38:19 -0600

    WIP:Finished person class working on instructor class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,23 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(attribs) {
+        this.name = attribs.name;
+        this.age = attribs.age;
+        this.location = attribs.location;
+    }
+    speak() {
+        console.log(`Hello my name is ${this.name} I am from ${this.location}`)
+    }
+}
+
+class Instructor extends Person {
+    constructor(inattribs) {
+        super(inattribs)
+        this.specialty = inattribs.specialty;
+        this.favLanguage = inattribs.favLanguage;
+        this.catchPhrase = inattribs.catchPhrase;
+    }
+    demo(subject) {
+        console.log(``)
+    } 
+}
